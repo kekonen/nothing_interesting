877fa6c6a9f225735780595c559836f420fbaf0e
Daniil Naumetc <daniil.naumetc@gmail.com>
2021-02-06 09:05:20 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -25,24 +25,24 @@ impl SecretStorage {
     // }
 
     fn add(&mut self, key: &str, value: &[u8]) {
-        self.storage.insert(key, value);
+        self.storage.insert(key.to_string(), value.to_vec());
     }
 
-    fn get(&mut self, key: &str) -> &[u8] {
-        self.storage.get(key);
+    fn get(&mut self, key: &str) -> Option<&Vec<u8>> {
+        self.storage.get(key)
     }
 }
 
 #[post("/<path>")]
 fn store(path: String, ss: State<SecretStorage>) -> String {
-    ss.add(path, vec![240, 159, 146, 150]);
+    ss.add(&path, &vec![240, 159, 146, 150]);
     format!("Post {}", path)
 }
 
 #[get("/<path>")]
 fn fetch(path: String, ss: State<SecretStorage>) -> String {
-    let value = ss.get(path);
-    format!("Get {}", std::str::from_utf8(value))
+    let value = ss.get(&path).unwrap();
+    format!("Get {}", std::str::from_utf8(value).unwrap())
 }
 
 fn main() {
